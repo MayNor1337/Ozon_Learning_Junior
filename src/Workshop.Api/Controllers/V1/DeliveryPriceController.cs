0b69dc113caf37d920e949f721e91f0351ee8755
MayNor <wen.net@yandex.ru>
2024-03-06 21:08:04 +0300

    create api contracts

---
+++ src/Workshop.Api/Controllers/V1/DeliveryPriceController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Workshop.Api.Requests.V1;
+using Workshop.Api.Responses.V1;
+
+namespace Workshop.Api.Controllers.V1;
+
+[ApiController]
+[Route("v1/[controller]")]
+public class DeliveryPriceController : ControllerBase
+{
+    [HttpPost]
+    public IEnumerable<CalculateResponse> Calculate(CalculateRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
